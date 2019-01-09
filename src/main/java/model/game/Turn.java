78c63e26fd4e850dfc16d247df4809eff68578d4
niko5297 <s185082@student.dtu.dk>
2019-01-09 17:10:51 +0100

    Made variables for Turn class.

--- src/main/java/model/game/Turn.java
+++ src/main/java/model/game/Turn.java
@@ -1,4 +1,41 @@
 package model.game;
 
+import model.board.Board;
+import model.board.Field;
+import model.cup.Cup;
+import model.player.Player;
+
 public class Turn {
+
+    /*
+    ------------ Fields ------------------
+    */
+
+    private Board playingBoard = new Board();
+
+    private Field turnField;
+
+    private int prePos, turnPos, postPos;
+
+    private int playerBalance, rollValue, boardPosition;
+
+    private String fieldName, fieldActionText;
+
+
+
+    /*
+    --------- Public Methods ----------
+    */
+
+    public void turn (Player player, Cup cup, Card card, Controller controller){
+
+
+
+
+    }
+
+
+    /*
+    --------- Support Methods ---------
+     */
 }
