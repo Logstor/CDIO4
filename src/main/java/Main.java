825d3d5f7b58a24f6d20e215743596faed7d841b
rasmussl <43174424+rasmussl@users.noreply.github.com>
2019-01-10 13:36:19 +0100

    Playing around with filePath in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -33,7 +33,9 @@ public class Main {
 		System.out.println(tempField);
 
 		System.out.println("Hello World");
-
+		String filePath;
+		filePath = ClassLoader.getSystemClassLoader().getResource("chanceCard.csv").getFile();
+	System.out.println(filePath);
 		//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 		HashMap<String,String> chanceCardMap = new HashMap<>();
 		Reader chanceCardReader = new Reader("src\\main\\resources\\","chanceCard_Packed.csv", ";");
