764c41bbaa477959ba8dda00e4bfc107b98e0422
Logstor <a.rottger_rydahl@live.dk>
2019-01-11 16:47:15 +0100

    Cleaned up Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,53 +1,13 @@
-import model.board.Board;
-import model.board.BoardManager;
-import model.board.Field;
-import model.chancecard.Deck;
-import model.chancecard.DeckManager;
-import model.reader.Reader;
-import view.gui.Gui;
-
-import java.util.HashMap;
+import controller.MainControl;
 
 /**
  * @author Alfred Röttger Rydahl
  */
-
 public class Main {
 
-	public static void main(String[] args) {
-
-		HashMap<String,String> boardMap = new HashMap<>();
-
-		Reader boardReader = new Reader( "board1.4_Packed.csv", ";");
-
-		boardReader.readFileIntoHashMap(boardMap);
-		System.out.println(boardReader.getFilePath());
-
-		System.out.println(boardMap.get("field1"));
-
-		System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
-
-		Board playingBoard = new Board();
-		BoardManager boardManager = new BoardManager();
-		boardManager.setupBoard(boardMap,playingBoard);
-
-		Field tempField = playingBoard.getBoard()[3];
-		System.out.println(tempField);
-
-		System.out.println("Hello World");
-
-		//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-		HashMap<String,String> chanceCardMap = new HashMap<>();
-		Reader chanceCardReader = new Reader("chanceCard_Packed.csv", ";");
-		chanceCardReader.readFileIntoHashMap(chanceCardMap);
-
-		Deck chanceCardDeck = new Deck();
-		DeckManager deckManager = new DeckManager();
-		deckManager.setupBeck(chanceCardMap,chanceCardDeck);
-
-		System.out.println(chanceCardDeck.getChanceCardDeck().get(3).toString());
-
-		Gui gui = new Gui(playingBoard.getBoard());
-		
+	public static void main(String[] args)
+	{
+		MainControl mainControl = new MainControl();
+		mainControl.launch();
 	}
 }
