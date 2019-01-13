a2e7e96f20dfed1e4c6266fe1cbdc5fc38dc1376
aksina <saaruppoulsen@gmail.com>
2019-01-13 17:47:21 +0100

    PlayerTest
    Test til player er done.
    Dog kan update balance gå i negativ.

---
+++ src/test/java/model/player/PlayerTest.java
@@ -0,0 +1,145 @@
+package model.player;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+
+    @Test
+    public void isHasLost() {
+        Player player = new Player("Test",3000,1);
+        player.setHasLost(true);
+        assertEquals(player.isHasLost(),true);
+    }
+
+    @Test
+    public void setHasLost() {
+        Player player = new Player("Test",3000,1);
+        player.setHasLost(false);
+        assertEquals(player.isHasLost(),false);
+    }
+
+    @Test
+    public void getAccount() {
+        Player player = new Player("Test",3000,1);
+        assertEquals(player.getAccount().getBalance(),3000);
+
+    }
+/*
+Aint in use
+    @Test
+    public void setAccount() {
+
+    }
+*/
+    @Test
+    public void getName() {
+        Player player = new Player("Test",3000,1);
+        assertEquals(player.getName(),"Test");
+    }
+
+    @Test
+    public void setName() {
+        Player player = new Player("Test",3000,1);
+        player.setName("Test2");
+        assertEquals(player.getName(),"Test2");
+    }
+
+    @Test
+    public void getBreweriesOwned() {
+        Player player = new Player("Test",3000,1);
+        player.setBreweriesOwned(1);
+        assertEquals(player.getBreweriesOwned(),1);
+    }
+
+    @Test
+    public void setBreweriesOwned() {
+        Player player = new Player("Test",3000,1);
+        player.setBreweriesOwned(2);
+        assertEquals(player.getBreweriesOwned(),2);
+    }
+
+    @Test
+    public void getBoatsOwned() {
+        Player player = new Player("Test",3000,1);
+        player.setBoatsOwned(2);
+        assertEquals(player.getBoatsOwned(),2);
+    }
+
+    @Test
+    public void setBoatsOwned() {
+        Player player = new Player("Test",3000,1);
+        player.setBoatsOwned(2);
+        assertEquals(player.getBoatsOwned(),2);
+    }
+
+    @Test
+    public void isInPrison() {
+        Player player = new Player("Test",3000,1);
+        assertEquals(player.isInPrison(),false);
+
+    }
+
+    @Test
+    public void setInPrison() {
+        Player player = new Player("Test",3000,1);
+        player.setInPrison(true);
+        assertEquals(player.isInPrison(),true);
+
+    }
+
+    @Test
+    public void getPosition() {
+        Player player = new Player("Test",3000,5);
+        assertEquals(player.getPosition(),5);
+    }
+
+    @Test
+    public void setPosition() {
+        Player player = new Player("Test",3000,1);
+        player.setPosition(11);
+        assertEquals(player.getPosition(),11);
+    }
+
+    @Test
+    public void getToken() {
+        Player player = new Player("Test",3000,1);
+        player.setToken("car");
+        assertEquals(player.getToken(),"car");
+    }
+
+    @Test
+    public void setToken() {
+        Player player = new Player("Test",3000,1);
+        player.setToken("car");
+        assertEquals(player.getToken(),"car");
+    }
+
+    @Test
+    public void updateBalance() {
+        Player player = new Player("Test",3000,1);
+        player.updateBalance(500);
+        assertEquals(player.getAccount().getBalance(),3500);
+        player.updateBalance(-3000);
+        assertEquals(player.getAccount().getBalance(),500);
+
+        /*
+        player.updateBalance(-600);
+        assertEquals(player.getAccount().getBalance(),0);
+        */
+    }
+
+    @Test
+    public void updatePosition() {
+        Player player = new Player("Test",3000,0);
+        player.updatePosition(11);
+        assertEquals(player.getPosition(),11);
+        player.updatePosition(-3);
+        assertEquals(player.getPosition(),8);
+        player.updatePosition(34);
+        assertEquals(player.getPosition(),2);
+        player.updatePosition(-5);
+        assertEquals(player.getPosition(),37);
+    }
+}
