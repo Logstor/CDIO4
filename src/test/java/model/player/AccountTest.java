c7ed99e09995c3e951eb8a455f37b56f8df3b936
aksina <saaruppoulsen@gmail.com>
2019-01-14 10:23:55 +0100

    update i field action på Boat og Tax og ændring i start field tekst

--- src/test/java/model/player/AccountTest.java
+++ src/test/java/model/player/AccountTest.java
@@ -2,26 +2,28 @@ package model.player;
 
 import org.junit.Test;
 
+import java.awt.*;
+
 import static org.junit.Assert.*;
 
 public class AccountTest {
 
     @Test
     public void getBalance() {
-        Player player = new Player("Test",3000,1);
+        Player player = new Player("Test", Color.blue, 3000,1);
         assertEquals(player.getAccount().getBalance(),3000);
     }
 
     @Test
     public void setBalance() {
-        Player player = new Player("Test",3000,1);
+        Player player = new Player("Test", Color.blue, 3000,1);
         player.getAccount().setBalance(2000);
         assertEquals(player.getAccount().getBalance(),2000);
     }
 
     @Test
     public void updateBalance() {
-        Player player = new Player("Test",3000,1);
+        Player player = new Player("Test", Color.blue, 3000,1);;
         player.getAccount().updateBalance(1000);
         assertEquals(player.getAccount().getBalance(),4000);
 
