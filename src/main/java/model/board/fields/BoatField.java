ac4fcd2411dc4f4a0abb9a195c3fea6cec09cded
aksina <saaruppoulsen@gmail.com>
2019-01-14 09:56:51 +0100

    update i field action på Boat og Tax og ændring i start field tekst

--- src/main/java/model/board/fields/BoatField.java
+++ src/main/java/model/board/fields/BoatField.java
@@ -42,6 +42,17 @@ public class BoatField extends Field {
 
     public void fieldAction (Player player) {
 
+        if (fieldOwner == null) {
+            actionText = "Du køber dette felt for " + fieldCost + " pengesedler\n";
+            setFieldOwner(player);
+            player.updateBalance(-fieldCost);
+        } else {
+            actionText = "Du er landet på "+ fieldOwner + "'s felt \n Du betaler "+ rentByOwnersNoOfBoats()
+                    + " pengesedler i husleje til "+ fieldOwner.getName();
+            player.updateBalance(-rentByOwnersNoOfBoats());
+            fieldOwner.updateBalance(rentByOwnersNoOfBoats());
+        }
+
     }
 
     public void fieldAction (Player player, Cup Cup) {}
@@ -49,6 +60,33 @@ public class BoatField extends Field {
     /*
     ---------------------- Support Methods ----------------------
      */
+    private int rentByOwnersNoOfBoats () {
+        int variableRent;
+        int OwnersNoOfBoats = fieldOwner.getBoatsOwned();
+
+        switch (OwnersNoOfBoats) {
+            case 1:
+                variableRent = 500;
+                break;
+
+            case 2:
+                variableRent = 1000;
+                break;
+
+            case 3:
+                variableRent = 2000;
+                break;
+
+            case 4:
+                variableRent = 4000;
+                break;
+
+            default:
+                variableRent = 666;
+                break;
+        }
+        return variableRent;
+    }
 
 
 }
