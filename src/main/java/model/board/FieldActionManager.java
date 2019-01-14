fe11fa0d40208e36c2a913527b23fbc551e233f3
nikolajwassmann <nwassmann@hotmail.com>
2019-01-14 11:21:29 +0100

    Edited some varibles in Player, and added methods for FieldActionManager

--- src/main/java/model/board/FieldActionManager.java
+++ src/main/java/model/board/FieldActionManager.java
@@ -15,6 +15,7 @@ public class FieldActionManager {
     ------------ Fields ------------------
     */
 
+    private Field field;
 
 
 
@@ -31,52 +32,57 @@ public class FieldActionManager {
 
     private void taxFieldAction(Player player, Account account, GuiController guiController){
 
-        guiController.showMessage(player + "Du er landet på et skattefelt");
-        guiController.showMessage("Du skal betale 4000 i skat");
-        if(account.getBalance()>4000) {
-            guiController.updateBalance(player, -4000);
+        if(player.getPosition()==5 || player.getPosition()==39) {
+            guiController.showMessage(player + "Du er landet på et skattefelt");
+            guiController.showMessage("Du skal betale 4000 i skat");
+            if (account.getBalance() > 4000) {
+                guiController.updateBalance(player, field.getFieldCost());
+            } else if (account.getBalance() < 4000) {
+                player.setHasLost(true);
+            }
         }
-        else if(account.getBalance()<4000){
+    }
 
+    private void boatFieldAction(Player player, GuiController guiController){
+
+
+
+    }
+
+    private void breweryFieldAction(Player player, GuiController guiController){
+
+    }
+
+    private void chanceFieldAction (Player player, GuiController guiController) {
+
+    }
+
+    private void parkingFieldAction (Player player, GuiController guiController) {
+        if (player.getPosition()==21) {
+            guiController.showMessage(player + "Du er landet på Gratis Parkering");
+            guiController.showMessage("Her må du parkere gratis");
         }
-    }
-
-    private void boatFieldAction(Player player, int position){
-
-    }
-
-    private void breweryFieldAction(Player player, int position){
-
-    }
-
-    private void chanceFieldAction (Player player, int position) {
-
-    }
-
-    private void parkingFieldAction (Player player, GuiController guiController){
-
-        guiController.showMessage(player + "Du er landet på Gratis Parkering");
-        guiController.showMessage("Her må du parkere gratis");
     }
 
     public void prisonFieldAction (Player player, GuiController guiController){
-
-        if(player.isInPrison()){
-            guiController.getUserButton1("Du sidder i fængsel og skal betale 1000" + player.getName(), "Betal 1000");
-            guiController.updateBalance(player,-1000);
-            player.setInPrison(false);
+        if(field.getFieldNo()==11) {
+            if (player.getPrisonStat() > 0) {
+                guiController.getUserButton1("Du sidder i fængsel og skal betale 1000" + player.getName(), "Betal 1000");
+                guiController.updateBalance(player, -1000);
+                player.setPrisonStat(0);
+            } else {
+                guiController.showMessage("Du er på besøg i fængslet");
+            }
         }
-
     }
 
-    private void propertyFieldAction (Player player, int position){
+    private void propertyFieldAction (Player player, GuiController guiController){
 
     }
 
 
     private void startField (Player player, GuiController guiController){
 
-        //Hvis vi antager at startfeltet er 0 i Field Arrayet
         if(player.getPosition()==0){
             guiController.updateBalance(player, +4000);
         }
