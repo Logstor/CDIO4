30c449e6451ba82e1f80eff755ee0c3cd2fb406d
Logstor <a.rottger_rydahl@live.dk>
2019-01-14 15:10:12 +0100

    FieldAction.java V1.0 done

--- src/main/java/controller/fieldManagement/FieldAction.java
+++ src/main/java/controller/fieldManagement/FieldAction.java
@@ -1,5 +1,11 @@
 package controller.fieldManagement;
 
+import controller.GuiController;
+import model.board.Field;
+import model.player.Player;
+
+import java.util.HashMap;
+
 /**
  * @author Alfred Röttger Rydahl
  */
@@ -8,11 +14,24 @@ public abstract class FieldAction {
 	/*
     ---------------------------------- Fields ----------------------------------
      */
+
+	protected GuiController guiController;
+	protected Player player;
+	protected Field field;
+	protected HashMap<String, String> messageMap;
     
     /*
     ------------------------------ Constructors --------------------------------
      */
-    
+
+	public FieldAction(GuiController guiController, Player player, Field field, HashMap<String, String> messageMap)
+	{
+		this.guiController = guiController;
+		this.player = player;
+		this.field = field;
+		this.messageMap = messageMap;
+	}
+
     /*
     ------------------------------ Properties ----------------------------------
      */
@@ -20,6 +39,8 @@ public abstract class FieldAction {
     /*
     ---------------------------- Public Methods --------------------------------
      */
+
+    public abstract void action ();
     
     /*
     ----------------------------- Support Methods ------------------------------
