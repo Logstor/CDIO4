e35a9a3c2c127c3a7fbaaeb74a0042729b33e26d
rasmussl <43174424+rasmussl@users.noreply.github.com>
2019-01-16 16:29:31 +0100

    Coded ToPrison.java and added it to ChanceCardAdder

--- src/main/java/controller/ChanceCardManageMent/ChanceCardActions/ToPrisonAction.java
+++ src/main/java/controller/ChanceCardManageMent/ChanceCardActions/ToPrisonAction.java
@@ -1,22 +1,34 @@
 package controller.ChanceCardManageMent.ChanceCardActions;
 
+import controller.ChanceCardManageMent.ChanceCardAction;
+import controller.GeneralActionController;
+import controller.GuiController;
+import model.board.Board;
+import model.board.Field;
+import model.player.Player;
+
+import java.util.HashMap;
+
 /**
  * @author Rasmus Sander Larsen
  * @date 16-01-2019
  */
-public class ToPrisonAction {
+public class ToPrisonAction extends ChanceCardAction {
 
     /*
     -------------------------- Fields --------------------------
      */
-    
-    
+
+    private Board board;
     
     /*
     ----------------------- Constructor -------------------------
      */
     
-    
+    public ToPrisonAction(GuiController guiController, HashMap<String,String> messageMap,
+                          GeneralActionController generalActionController, Board board) {
+        super(guiController,messageMap,generalActionController);
+        this.board = board;
     
     /*
     ------------------------ Properties -------------------------
