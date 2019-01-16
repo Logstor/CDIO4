01860087e8dc4f305c30164e902c074bc95564fa
rasmussl <43174424+rasmussl@users.noreply.github.com>
2019-01-16 16:19:29 +0100

    Created ToPrison.java in ChanceCard Package. Created ToPrison.java in ChanceCardAction Package.

---
+++ src/main/java/controller/ChanceCardManageMent/ChanceCardActions/ToPrisonAction.java
@@ -0,0 +1,41 @@
+package controller.ChanceCardManageMent.ChanceCardActions;
+
+/**
+ * @author Rasmus Sander Larsen
+ * @date 16-01-2019
+ */
+public class ToPrisonAction {
+
+    /*
+    -------------------------- Fields --------------------------
+     */
+    
+    
+    
+    /*
+    ----------------------- Constructor -------------------------
+     */
+    
+    
+    
+    /*
+    ------------------------ Properties -------------------------
+     */
+
+    // <editor-folder desc="Properties"
+
+
+    // </editor-folder>
+    
+    /*
+    ---------------------- Public Methods -----------------------
+     */
+    
+    
+    
+    /*
+    ---------------------- Support Methods ----------------------
+     */
+
+
+}
---
+++ src/main/java/model/chancecard/cards/ToPrison.java
@@ -0,0 +1,41 @@
+package model.chancecard.cards;
+
+/**
+ * @author Rasmus Sander Larsen
+ * @date 16-01-2019
+ */
+public class ToPrison {
+
+    /*
+    -------------------------- Fields --------------------------
+     */
+    
+    
+    
+    /*
+    ----------------------- Constructor -------------------------
+     */
+    
+    
+    
+    /*
+    ------------------------ Properties -------------------------
+     */
+
+    // <editor-folder desc="Properties"
+
+
+    // </editor-folder>
+    
+    /*
+    ---------------------- Public Methods -----------------------
+     */
+    
+    
+    
+    /*
+    ---------------------- Support Methods ----------------------
+     */
+
+
+}
