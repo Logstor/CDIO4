064921cba433a9b3ed8db3dec59cda8a52181f0d
rasmussl <43174424+rasmussl@users.noreply.github.com>
2019-01-18 12:53:01 +0100

    Added all logic for new ChanceCard:
    Out of Prison

---
+++ src/main/java/model/chancecard/cards/OutOfPrison.java
@@ -0,0 +1,46 @@
+package model.chancecard.cards;
+
+import model.chancecard.CardTypeEnum;
+import model.chancecard.ChanceCard;
+
+/**
+ * @author Rasmus Sander Larsen
+ * @date 18-01-2019
+ */
+public class OutOfPrison extends ChanceCard {
+
+    /*
+    -------------------------- Fields --------------------------
+     */
+    
+    
+    
+    /*
+    ----------------------- Constructor -------------------------
+     */
+    
+    public OutOfPrison (CardTypeEnum cardType, String title) {
+        super(cardType,title);
+    }
+    
+    /*
+    ------------------------ Properties -------------------------
+     */
+
+    // <editor-folder desc="Properties"
+
+
+    // </editor-folder>
+    
+    /*
+    ---------------------- Public Methods -----------------------
+     */
+    
+    
+    
+    /*
+    ---------------------- Support Methods ----------------------
+     */
+
+
+}
