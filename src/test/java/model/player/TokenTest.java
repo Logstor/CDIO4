c6dae1b9fcd2930b6876b311f677d47711e07649
aksina <saaruppoulsen@gmail.com>
2019-01-18 10:48:07 +0100

    die test implantet

---
+++ src/test/java/model/player/TokenTest.java
@@ -0,0 +1,40 @@
+package model.player;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TokenTest {
+
+    @Test
+    public void getCarType() {
+    }
+
+    @Test
+    public void setCarType() {
+    }
+
+    @Test
+    public void getCarPattern() {
+    }
+
+    @Test
+    public void setCarPattern() {
+    }
+
+    @Test
+    public void getCarColor() {
+    }
+
+    @Test
+    public void setCarColor() {
+    }
+
+    @Test
+    public void getPatternColor() {
+    }
+
+    @Test
+    public void setPatternColor() {
+    }
+}
