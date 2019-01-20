6c2ba94e3ead43199a51ec794f74c4619f908106
rasmussl <43174424+rasmussl@users.noreply.github.com>
2019-01-20 23:09:54 +0100

    Fixed remove player, from Players to PlayerArraylist

--- src/main/java/controller/MainControl.java
+++ src/main/java/controller/MainControl.java
@@ -245,7 +245,7 @@ public class MainControl {
 	 * Runs through all Players in Player[] players and remove the player from the Arraylist if Player Has Lost.
 	 */
 	private void removesHasLostPlayersFromPlayerArrayList () {
-		for (Player player : pl) {
+		for (Player player : playerArrayList) {
 			if (player.isHasLost()) {
 				playerArrayList.remove(player);
 			}
